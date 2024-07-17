d8a627ffa42da4ed5f9ec0391d5987e12af93359
Merlin Unterfinger <merlin.unterfinger@sbb.ch>
2024-07-17 16:15:29 +0200

    Enable jupyter notebooks, update test data

--- rssched/data/access.py
+++ rssched/data/access.py
@@ -7,7 +7,13 @@ class PkgDataAccess:
         pass
 
     @staticmethod
+    def locate_request() -> Path:
+        data_folder = resources.files("rssched.data")
+        file_path = data_folder / "small_test_request.json"
+        return file_path
+
+    @staticmethod
     def locate_response() -> Path:
         data_folder = resources.files("rssched.data")
-        file_path = data_folder / "output_small_test_input.json"
+        file_path = data_folder / "small_test_response.json"
         return file_path
